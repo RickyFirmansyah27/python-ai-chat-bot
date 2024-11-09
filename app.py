b902c420f4c1b4af051ca7adab3ed3a860076866
Ricky Firmansyah <rickyfirmansyah2707@gmail.com>
2024-11-09 11:48:10 +0700

    taekout import dotenv

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pdfplumber
 import os
-from dotenv import load_dotenv
 from langchain.chains import LLMChain
 from langchain_core.prompts import (
     ChatPromptTemplate,
